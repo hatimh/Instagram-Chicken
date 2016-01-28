06556e20d2f71d1e62708aa19f566360a41353b0
ClockKnight <scottymcphee@gmail.com>
2016-01-28 12:43:24 -0700

    changed out login form for a link to Instagram oauth

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,20 @@
+# Homepage (Root path)
+require "instagram"
+
 enable :sessions
+
+CALLBACK_URL = "http://localhost:3000/oauth/callback"
+
+Instagram.configure do |config|
+  config.client_id = "4a9c8bcaff1b4b03901e20bb5777d8bd"
+  config.client_secret = "1b574823e66d41dbbbb2a84b18646ddd"
+end
 
 # get methods #
 get '/' do
   erb :'/index'
+end
+
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
 end
